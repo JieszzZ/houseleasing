f1b547abc5b3f0fb3f7ec71472a605c657201a32
lijiayu51578 <1092290564@qq.com>
2019-06-24 21:01:19 +0800

    ''

--- src/main/java/com/mokelock/houseleasing/IPFS/HouseServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.mokelock.houseleasing.services.servicesImpl;
-
-import com.mokelock.houseleasing.services.HouseService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class HouseServiceImpl implements HouseService {
-}
