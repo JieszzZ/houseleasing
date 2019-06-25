7c83f2c7ff1c19ef207613a1bed2f5687ae936d3
lijiayu51578 <1092290564@qq.com>
2019-06-25 21:35:02 +0800

    merge

--- src/main/java/com/mokelock/houseleasing/services/servicesImpl/UserServicesImpl.java
+++ src/main/java/com/mokelock/houseleasing/services/servicesImpl/UserServicesImpl.java
@@ -1,12 +1,59 @@
 package com.mokelock.houseleasing.services.servicesImpl;
 
+import com.mokelock.houseleasing.model.UserModel.User;
+import com.mokelock.houseleasing.model.UserModel.modifyUser;
+import com.mokelock.houseleasing.model.UserModel.record;
 import com.mokelock.houseleasing.services.UserService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServicesImpl implements UserService {
     @Override
-    public boolean signIn() {
+    public int getBalance(String _username) {
+        return 0;
+    }
+
+    @Override
+    public boolean getUser(User _one, String _username) {
         return false;
     }
+
+    @Override
+    public boolean postAccount(String _username, int _money) {
+        return false;
+    }
+
+    @Override
+    public ArrayList<record> getRecords(String _username) {
+        return null;
+    }
+
+    @Override
+    public boolean postCredit(User _old, short _credit) {
+        return false;
+    }
+
+    @Override
+    public boolean postGender(User _old, byte _gender) {
+        return false;
+    }
+
+    @Override
+    public boolean postPassword(User _old, String _password) {
+        return false;
+    }
+
+    @Override
+    public boolean postPhone(User _old, String _phone) {
+        return false;
+    }
+
+    @Override
+    public boolean postUser(User _old, modifyUser _modified) {
+        return false;
+    }
+
+
 }
