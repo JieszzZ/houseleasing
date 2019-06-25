231ec1d82033ad7918e5f2e8214f05889427940b
lijiayu51578 <1092290564@qq.com>
2019-06-25 21:06:15 +0800

    IPFS接口简单实现上传和下载，java读取与存取文件

---
+++ src/main/java/com/mokelock/houseleasing/IPFS/File_Write_Test.java
@@ -0,0 +1,37 @@
+package com.mokelock.houseleasing.IPFS;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class File_Write_Test {
+    public static void main(String[] args) {
+        File file = null;
+        FileWriter fw= null;
+        file = new File("C:\\Users\\10922\\Desktop\\demo.txt");
+        try {
+            if (!file.exists()){
+                file.createNewFile();
+            }
+            fw = new FileWriter(file);
+            for (int i = 1;i <= 3000; i++){
+                fw.write("eid"+i+",");//向文件写内容
+                fw.write("name"+i+",\r\n");//加上换行
+                fw.flush();
+            }
+            System.out.println("写数据成功！");
+        }catch (IOException e){
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            if (fw != null){
+                try {
+                    fw.close();
+            }catch (IOException e){
+                e.printStackTrace();
+                }
+            }
+        }
+    }
+}
