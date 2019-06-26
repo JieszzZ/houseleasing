ab6e6e1ed24d66125abeea40b3052abe3d2010c8
IsaacDiane <1239062183@qq.com>
2019-06-26 19:38:37 +0800

    Changes to be committed: 	modified:   src/main/java/com/mokelock/houseleasing/IPFS/Table.java 	modified:   src/main/java/com/mokelock/houseleasing/services/servicesImpl/HouseServiceImpl.java

--- src/main/java/com/mokelock/houseleasing/IPFS/Table.java
+++ src/main/java/com/mokelock/houseleasing/IPFS/Table.java
@@ -13,9 +13,10 @@ public interface Table {
     * user-eth表：{"user_name","eth_id"}
     * house-eth-info表,注：本表没有state属性，根据表的命名来判断房子state
     * 比如在搜索state="0"的房子时，path=/table/for/house/state/0.txt
-    * 表结构:{"house_id",}
+    * 表结构:{"house_id","eth_id","lease_inter","house_type","lease_type","provi","city","sector","commu_name"}
     * insert时，参数顺序不能出错，query时无所谓。
-    *
+    *insert之前要调用query来查询houseid，然后判断返回值.size()是否为0，若不为0则重复，不能插入。
+    * 文本写入编码一律为utf8，读文件时注意编码。
     *  */
     void insert(House house_obj,String eth_id,String path);//插入查重必须在前面完成，插入之前要先运行一次query house_id，并判断返回值的size是否为0
 }
