6bdecc39a73fe235ab7672c2a80ebc801ebbd2fe
lijiayu51578 <1092290564@qq.com>
2019-07-03 14:28:39 +0800

    merge

--- src/main/java/com/mokelock/houseleasing/IPFS/IpfsImpl/IPFS_SERVICE_IMPL.java
+++ src/main/java/com/mokelock/houseleasing/IPFS/IpfsImpl/IPFS_SERVICE_IMPL.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 public class IPFS_SERVICE_IMPL implements IPFS_SERVICE {
 
-    public static IPFS ipfs = new IPFS("/ip4/127.0.0.1/tcp/5001");//ipfs的服务器地址和端口
+    public static IPFS ipfs = new IPFS("/ip4/0.0.0.0/tcp/8080");//ipfs的服务器地址和端口
     public static String upload(String filePathName) throws IOException {
         //filePathName指的是文件(夹)的上传路径+文件名，如D:/1.png
         NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File(filePathName));
